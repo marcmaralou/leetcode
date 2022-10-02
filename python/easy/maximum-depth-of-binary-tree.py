4688a0a972ce84ebc176ce26f86b2f4500b465e5
Marc Maravilla-Louie <102846078+marcmaralou@users.noreply.github.com>
2022-10-02 12:26:42 -0700

    Update maximum-depth-of-binary-tree.py

--- python/easy/maximum-depth-of-binary-tree.py
+++ python/easy/maximum-depth-of-binary-tree.py
@@ -42,3 +42,19 @@ class Solution:
             level += 1
         
         return level
+
+# ITERATIVE
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        ans = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                ans = max(ans, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        
+        return ans
