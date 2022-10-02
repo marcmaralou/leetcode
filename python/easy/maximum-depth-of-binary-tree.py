d920faa688ef51dc0e35c4abc50248019caa4a6c
Marc Maravilla-Louie <102846078+marcmaralou@users.noreply.github.com>
2022-10-02 09:55:05 -0700

    Update maximum-depth-of-binary-tree.py

--- python/easy/maximum-depth-of-binary-tree.py
+++ python/easy/maximum-depth-of-binary-tree.py
@@ -16,8 +16,29 @@
 #         self.left = left
 #         self.right = right
 
+# RECURSION
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root == None:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        
+        return level
