2fea6be44fafcf7b1445f64059f501b936481dcb
Marc Maravilla-Louie <102846078+marcmaralou@users.noreply.github.com>
2022-11-25 10:40:38 -0800

    Update clone-graph.py

--- python/medium/clone-graph.py
+++ python/medium/clone-graph.py
@@ -2,10 +2,10 @@
 # Return a deep copy (clone) of the graph.
 # Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
 
-class Node {
-    public int val;
-    public List<Node> neighbors;
-}
+# class Node {
+#     public int val;
+#     public List<Node> neighbors;
+# }
 
 # Test case format:
 # For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
@@ -31,13 +31,13 @@ class Node {
 # Output: []
 # Explanation: This an empty graph, it does not have any nodes.
 
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
+# """
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, val = 0, neighbors = None):
+#         self.val = val
+#         self.neighbors = neighbors if neighbors is not None else []
+# """
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
