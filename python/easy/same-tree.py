48f53c6d91943f6b69dc3e28dd0d1b2eb0ec9a26
Marc Maravilla-Louie <102846078+marcmaralou@users.noreply.github.com>
2022-12-30 09:57:41 -0800

    Update same-tree.py

--- python/easy/same-tree.py
+++ python/easy/same-tree.py
@@ -21,10 +21,11 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None and q == None:
+        if not p and not q:
             return True
-        if p == None or q == None:
+        if not p or not q:
             return False
         if p.val != q.val:
             return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        if p.val == q.val: # O(n) time, worst case going through every node of both trees
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
